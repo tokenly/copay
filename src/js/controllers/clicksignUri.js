800dae32609cfd82aecde695a2685767aeb64ef6
cryptonaut <nrathman@ironcladtech.ca>
2016-08-21 14:57:33 -0700

    fixed issue on address dropdown for click-to-sign and cleaned up code a bit

--- src/js/controllers/clicksignUri.js
+++ src/js/controllers/clicksignUri.js
@@ -1,24 +1,20 @@
 'use strict';
 angular.module('copayApp.controllers').controller('clicksignUriController',
   function($rootScope, $scope, $stateParams, $location, $timeout, profileService, configService, lodash, signService, go, bitcore) {
-
+    //setup variables
 	var fc = profileService.focusedClient;
-	this.fc = fc;
-	this.lodash = lodash;
-	this.$scope = $scope;
 	var walletId = fc.credentials.walletId;
 	var config = configService.getSync();
-	var addresses = [];
 	var formScope = false;
 	
 	config.aliasFor = config.aliasFor || {};
 	$scope.alias = config.aliasFor[walletId] || fc.credentials.walletName;
-
+    
+    //run ng-init to assign page vars
     this.init = function() {
       var query = [];
       this.clicksignURI = $stateParams.url;
       var uri = this.parseURI(this.clicksignURI);
-      console.log('testing click to sign..');
       if(!uri) {
 		  console.log('Invalid click-to-sign URI');
 		  this.error = true;
@@ -41,58 +37,39 @@ angular.module('copayApp.controllers').controller('clicksignUriController',
 	  }
 	  $scope.callback = uri.callback;
     };
-
+    
+    //load list of wallets
     this.getWallets = function(network) {
       return profileService.getWallets(network);
     };
     
-    this.parseURI = function(uri) {
-		var split = uri.split(':');
-		if(split.length == 2){
-			var querystring = split[1].split('?');
-			if(querystring[0] == 'sign' && querystring.length == 2){
-				var query = this.parseQueryString('?' + querystring[1]);
-				return query;
-			}
-		}
-		return false;
-	};
-	
-	this.parseQueryString = function (qstr) {
-        var query = {};
-        var a = qstr.substr(1).split('&');
-        for (var i = 0; i < a.length; i++) {
-            var b = a[i].split('=');
-            query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    //load in list of wallet addresses
+    var addresses = [];
+    fc.getMainAddresses({
+        doNotVerify: true
+    }, function(err, x) {
+        if (x.length > 0) {
+            lodash.each(x, function(a) {
+                addresses.push(a);
+            });
+        } else {
+            console.log('* No addresses.');
         }
-        return query;
-    };
+        addresses = addresses.reverse();
+        $scope.addresses = addresses;
+        $scope.keySelector = addresses[0];
+    });	     
+
     
+    //set the form scope on init
     this.setFormScope = function (form) {
-		console.log(form);
-		var addresses = [];
-		var lodash = this.lodash;
-		fc.getMainAddresses({
-			doNotVerify: true
-		}, function(err, x) {
-			if (x.length > 0) {
-				lodash.each(x, function(a) {
-					addresses.push(a);
-				});
-			} else {
-				console.log('* No addresses.');
-			}
-			addresses = addresses.reverse();
-			console.log(addresses);
-		});	  		
-		this.addresses = addresses;
-		this.$scope.addresses = addresses;
 		this.formScope = form;
 	};
 	
-	$scope.sign = function(index, message) {
+    
+    //perform the signature creation and callback
+	$scope.sign = function(address, message) {
 		var toSign = message;
-		var address = this.addresses[index];
 		var did_sign = false;
 		signService.deriveKeyAndSign(address, fc, toSign, function(signed) {
 			$scope.signature = signed;
@@ -115,7 +92,31 @@ angular.module('copayApp.controllers').controller('clicksignUriController',
 			window.location.href = callback;
 		}
 	};	
+    
+    //get the query parameters out of a URI
+    this.parseURI = function(uri) {
+		var split = uri.split(':');
+		if(split.length == 2){
+			var querystring = split[1].split('?');
+			if(querystring[0] == 'sign' && querystring.length == 2){
+				var query = this.parseQueryString('?' + querystring[1]);
+				return query;
+			}
+		}
+		return false;
+	};
 	
+	this.parseQueryString = function (qstr) {
+        var query = {};
+        var a = qstr.substr(1).split('&');
+        for (var i = 0; i < a.length; i++) {
+            var b = a[i].split('=');
+            query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+        }
+        return query;
+    };
+	
+    //close window when they hit cancel
 	this.closeWindow = function() {
 		window.close();
 	}
