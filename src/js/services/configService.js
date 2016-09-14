65860e21aa12b2be3896b98f0f5c35209c77317e
Devon Weller <devon@tokenly.co>
2016-09-14 13:37:05 -0500

    Default to BTC unit instead of bits

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -23,10 +23,10 @@ angular.module('copayApp.services').factory('configService', function(storageSer
       reconnectDelay: 5000,
       idleDurationMin: 4,
       settings: {
-        unitName: 'bits',
-        unitToSatoshi: 100,
-        unitDecimals: 2,
-        unitCode: 'bit',
+        unitName: 'BTC',
+        unitToSatoshi: 100000000,
+        unitDecimals: 8,
+        unitCode: 'btc',
         alternativeName: 'US Dollar',
         alternativeIsoCode: 'USD',
       }
